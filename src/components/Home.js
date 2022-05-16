d462bc4490b813ad623d43a47070e33c018bf80e
Wangchowchow <wangsahioe@gmail.com>
2022-05-16 14:52:04 +0700

    update home page content

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,11 +3,11 @@ import React from "react";
 function Home() {
     return (
         <div className="home-page">
-            <h1>LOVE YOUR DOGGO</h1>
-            <img src="./assets/siberianhusky.jpg" alt="dog"></img>
+            <h1>Love Your Doggo</h1>
+            <img src="../assets/siberianhusky.jpg" alt="dog"></img>
             <h2>Show affection for your beloved doggo!</h2>
             <p>One stop solution for all your doggo needs</p>
-            <p>Note: Best deal ends today, spend $100 or more for free shipping.</p>
+            <p>Note: Best deal ends today, free shipping with no minimum spend.</p>
             <h3>Start Shopping</h3>
         </div>
     )
