a3bc94d9b7c1e6ed0a66cd83970156c6a0a75e45
Wangchowchow <wangsahioe@gmail.com>
2022-05-16 11:46:58 +0700

    add home page

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+const Home = () => {
+    return (
+        <div className="home-page">
+            <h1>LOVE YOUR DOGGO</h1>
+            <img src="./assets/siberianhusky.jpg" alt="dog"></img>
+            <h2>Show affection for your beloved doggo!</h2>
+            <p>One stop solution for all your doggo needs</p>
+            <p>Note: Best deal ends today, spend $100 or more for free shipping.</p>
+            <h3>Start Shopping</h3>
+        </div>
+    )
+}
