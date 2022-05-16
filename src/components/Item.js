ed3c1afe4277e32b532f6c166c0dce33b29f0735
Wangchowchow <wangsahioe@gmail.com>
2022-05-16 12:33:15 +0700

    add item component

---
+++ src/components/Item.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { useState } from "react";
+
+function Item({ name, price, src, add }) {
+    const [quantity, setQuantity] = useState(1);
+
+    const increase = () => {
+        setQuantity((quantity+1));
+    };
+    const decrease = () => {
+        setQuantity((quantity-1));
+    };
+    const addToCart = () => {
+        const item = { name, price, quantity};
+        add(item);
+    };
+
+    return (
+        <div className="item-container">
+            <h3>{name}</h3>
+            <img src={src} alt={name}></img>
+            <h4>{price}</h4>
+            <div>
+                <button onClick={addToCart}>Add to Cart</button>
+                <button onClick={decrease}>-</button>
+                <span>{quantity}</span>
+                <button onClick={increase}>+</button>
+            </div>
+        </div>
+    )
+}
+
+export default Item;
