b7d50d84147196eeb6e6302ec03d7748c22cddf1
Wangchowchow <wangsahioe@gmail.com>
2022-05-17 19:46:41 +0700

    displays number of items currently in cart

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-function Nav() {
+function Nav({cartLength}) {
     return (
         <nav>
             <Link to="/">LYD</Link>
             <Link to="/">Home</Link>
             <Link to="/products">Products</Link>
-            <Link to="shopping-cart">Shopping Cart</Link>
+            <Link to="shopping-cart">Shopping Cart ({cartLength})</Link>
         </nav>
     );
 };
