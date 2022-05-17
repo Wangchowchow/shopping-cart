31443cbd4a59cb2584c84dc5f186c13aebe26968
Wangchowchow <wangsahioe@gmail.com>
2022-05-17 14:37:12 +0700

    add shopping functionality

--- src/App.js
+++ src/App.js
@@ -13,16 +13,50 @@ function App() {
     const [cart, setCart] = useState([]);
 
     const add = (newItem) => {
-        setCart((item) => [...item, newItem]);
+        let isDuplicate = false;
+    
+        cart.forEach((cartItem, i) => {
+            if (newItem.name === cartItem.name) {
+                const newCart = [...cart];
+                const updateItem = newCart[i];
+                updateItem.quantity += newItem.quantity;
+                newCart[i] = updateItem;
+                setCart(newCart);
+                isDuplicate = true;
+            }
+        });
+
+        if (!isDuplicate) {
+            setCart((prevCart) => {
+                return [...prevCart, newItem];
+            });
+        }
     };
+
+    const remove = (item) => {
+        const newCart = [...cart];
+        newCart.forEach((cartItem, i) => {
+            if (item === cartItem.name) {
+                newCart.splice(i, 1);
+            }
+        });
+
+        setCart(newCart);
+    };
+
+    const emptyCart = () => {
+        setCart([]);
+    }
+
+    useEffect(() => { }, [cart]);
 
     return (
         <BrowserRouter>
             <Nav></Nav>
             <Routes>
                 <Route path="/" element={<Home></Home>}></Route>
-                <Route path="/products" element={<Products></Products>}></Route>
-                <Route path="/shopping-cart" element={<ShoppingCart></ShoppingCart>}></Route>
+                <Route path="/products" element={<Products add={add}></Products>}></Route>
+                <Route path="/shopping-cart" element={<ShoppingCart items={cart} remove={remove} emptyCart={emptyCart}></ShoppingCart>}></Route>
             </Routes>
         </BrowserRouter>
     )
