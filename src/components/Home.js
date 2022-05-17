4b5f80e3422ac28f610b9755cda71804f20ae8d2
Wangchowchow <wangsahioe@gmail.com>
2022-05-17 14:40:30 +0700

    fix quantity going below 1 when minus button is clicked several times

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+import siberianHusky from "../assets/siberianhusky.jpeg"
 
 function Home() {
     return (
         <div className="home-page">
             <h1>Love Your Doggo</h1>
-            <img src="../assets/siberianhusky.jpg" alt="dog"></img>
-            <h2>Show affection for your beloved doggo!</h2>
+            <img src={siberianHusky} alt="dog"></img>
+            <h2>Show affection to your beloved doggo!</h2>
             <p>One stop solution for all your doggo needs</p>
             <p>Note: Best deal ends today, free shipping with no minimum spend.</p>
-            <h3>Start Shopping</h3>
+            <Link to="/products">Start Shopping</Link>
         </div>
     )
 }
